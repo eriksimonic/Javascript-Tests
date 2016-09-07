44d7dc0a9c9774c06c000b919929df32d8352eb7
Erik Simonič <erik.simonic@gmail.com>
2016-09-07 22:47:21 +0200

    Scrolling form. Select input on scroll to screen center, and advance with Enter on input keydown

--- BaseProject/scrolling-form.js
+++ BaseProject/scrolling-form.js
@@ -1,53 +1,40 @@
-﻿((w: Window, docElm: Element) => {
-    let previews = null;
-    let elements = $('div.content');
-
-    elements.each((i, e) => $(e).data('index', i));
-
-    let viewportHeight, middle, topIndex, bottomIndex;
-
-    let setElementActive = (e: Element) => {
+(function (w, docElm) {
+    var previews = null;
+    var elements = $('div.content');
+    elements.each(function (i, e) { return $(e).data('index', i); });
+    var viewportHeight, middle, topIndex, bottomIndex;
+    var setElementActive = function (e) {
         $(e).addClass('active').find('input').focus();
     };
-
-    
-
-    let scrollElementIntoView = (e: Element) => {
-        let bRect = e.getBoundingClientRect();
-        let cElement = e;
-    
+    var scrollElementIntoView = function (e) {
+        var bRect = e.getBoundingClientRect();
+        var cElement = e;
         viewportHeight = docElm.clientHeight;
         middle = viewportHeight / 2;
         topIndex = middle / 2;
-        let scTop = document.body.scrollTop;
+        var scTop = document.body.scrollTop;
         scTop = document.body.scrollTop + (bRect.top - middle);
         scTop = scTop + ($(e).height() / 2);
-        $('html, body').stop(true, false).animate({ scrollTop: scTop}, 450, () => {
+        $('html, body').stop(true, false).animate({ scrollTop: scTop }, 450, function () {
             setElementActive(cElement);
         });
-
-      
     };
-
-    elements.on('keydown', (e: JQueryEventObject) => {
+    elements.on('keydown', function (e) {
         if (e.keyCode == 13) {
-            let index = $(e.delegateTarget).data('index');
+            var index = $(e.delegateTarget).data('index');
             $(e.delegateTarget).removeClass('active');
-            index = index+1;
+            index = index + 1;
             console.log(index);
             if (index >= elements.length)
                 index = 0;
             scrollElementIntoView(elements[index]);
         }
     });
-
-    let findMiddleElement = (function (docElm) {
-        
-         viewportHeight = docElm.clientHeight;
-         middle = viewportHeight / 2;
-         topIndex = middle / 2;
-         bottomIndex = middle / 1.2;
-
+    var findMiddleElement = (function (docElm) {
+        viewportHeight = docElm.clientHeight;
+        middle = viewportHeight / 2;
+        topIndex = middle / 2;
+        bottomIndex = middle / 1.2;
         return function (e) {
             var middleElement;
             if (e && e.type == 'resize') {
@@ -56,8 +43,8 @@
                 topIndex = middle / 2;
                 bottomIndex = middle / 1.2;
             }
-            elements.each((i, e) => {
-                let offset = e.getBoundingClientRect();
+            elements.each(function (i, e) {
+                var offset = e.getBoundingClientRect();
                 if (offset.top > topIndex && offset.top < bottomIndex) {
                     middleElement = e;
                     return false; // stop iteration
@@ -67,22 +54,16 @@
             //{
             //    return false;
             //}
-           // console.log(previews, middleElement);
+            // console.log(previews, middleElement);
             if (middleElement) {
                 if (previews)
                     $(previews).removeClass("active");
-
                 previews = middleElement;
                 setElementActive(middleElement);
-                
             }
-         }
+        };
     })(docElm);
-
-
     $(w).on('scroll resize', findMiddleElement).resize();
-
-
     //let scrollHeight = 0;
     //let h1 = document.getElementsByTagName("h1")[0];
     //document.body.style.height = '50000px';
@@ -94,13 +75,11 @@
     //        }
     //        h1.style.top =
     //            `${document.body.scrollTop}px`;
-
     //        console.log(`
     //        ${document.body.scrollTop},
     //        ${document.body.clientHeight},
     //        ${window.innerHeight},
     //        ${window.outerHeight},
-
     //        `);
     //    }
     //    else if (document.body.scrollTop <= 100)
@@ -108,7 +87,7 @@
     //        h1.style.position = "static";
     //    }
     //    //console.log(document.body.scrollTop);
-
     //};
     //setInterval(myFraneticFunction, 10);
-})(window, document.documentElement)
+})(window, document.documentElement);
+//# sourceMappingURL=scrolling-form.js.map
--- BaseProject/scrolling-form.ts
+++ BaseProject/scrolling-form.ts
@@ -1,40 +1,51 @@
-(function (w, docElm) {
-    var previews = null;
-    var elements = $('div.content');
-    elements.each(function (i, e) { return $(e).data('index', i); });
-    var viewportHeight, middle, topIndex, bottomIndex;
-    var setElementActive = function (e) {
+﻿((w: Window, docElm: Element) => {
+    let previews = null;
+    let elements = $('div.content');
+
+    elements.each((i, e) => $(e).data('index', i));
+
+    let viewportHeight, middle, topIndex, bottomIndex;
+
+    let setElementActive = (e: Element) => {
         $(e).addClass('active').find('input').focus();
     };
-    var scrollElementIntoView = function (e) {
-        var bRect = e.getBoundingClientRect();
-        var cElement = e;
+
+    
+
+    let scrollElementIntoView = (e: Element) => {
+        let bRect = e.getBoundingClientRect();
+        let cElement = e;
+    
         viewportHeight = docElm.clientHeight;
         middle = viewportHeight / 2;
         topIndex = middle / 2;
-        var scTop = document.body.scrollTop;
+        let scTop = document.body.scrollTop;
         scTop = document.body.scrollTop + (bRect.top - middle);
         scTop = scTop + ($(e).height() / 2);
-        $('html, body').stop(true, false).animate({ scrollTop: scTop }, 450, function () {
+        $('html, body').stop(true, false).animate({ scrollTop: scTop}, 450, () => {
             setElementActive(cElement);
         });
     };
-    elements.on('keydown', function (e) {
+
+    elements.on('keydown', (e: JQueryEventObject) => {
         if (e.keyCode == 13) {
-            var index = $(e.delegateTarget).data('index');
+            let index = $(e.delegateTarget).data('index');
             $(e.delegateTarget).removeClass('active');
-            index = index + 1;
+            index = index+1;
             console.log(index);
             if (index >= elements.length)
                 index = 0;
             scrollElementIntoView(elements[index]);
         }
     });
-    var findMiddleElement = (function (docElm) {
-        viewportHeight = docElm.clientHeight;
-        middle = viewportHeight / 2;
-        topIndex = middle / 2;
-        bottomIndex = middle / 1.2;
+
+    let findMiddleElement = (function (docElm) {
+        
+         viewportHeight = docElm.clientHeight;
+         middle = viewportHeight / 2;
+         topIndex = middle / 2;
+         bottomIndex = middle / 1.2;
+
         return function (e) {
             var middleElement;
             if (e && e.type == 'resize') {
@@ -43,8 +54,8 @@
                 topIndex = middle / 2;
                 bottomIndex = middle / 1.2;
             }
-            elements.each(function (i, e) {
-                var offset = e.getBoundingClientRect();
+            elements.each((i, e) => {
+                let offset = e.getBoundingClientRect();
                 if (offset.top > topIndex && offset.top < bottomIndex) {
                     middleElement = e;
                     return false; // stop iteration
@@ -54,16 +65,22 @@
             //{
             //    return false;
             //}
-            // console.log(previews, middleElement);
+           // console.log(previews, middleElement);
             if (middleElement) {
                 if (previews)
                     $(previews).removeClass("active");
+
                 previews = middleElement;
                 setElementActive(middleElement);
+                
             }
-        };
+         }
     })(docElm);
+
+
     $(w).on('scroll resize', findMiddleElement).resize();
+
+
     //let scrollHeight = 0;
     //let h1 = document.getElementsByTagName("h1")[0];
     //document.body.style.height = '50000px';
@@ -75,11 +92,13 @@
     //        }
     //        h1.style.top =
     //            `${document.body.scrollTop}px`;
+
     //        console.log(`
     //        ${document.body.scrollTop},
     //        ${document.body.clientHeight},
     //        ${window.innerHeight},
     //        ${window.outerHeight},
+
     //        `);
     //    }
     //    else if (document.body.scrollTop <= 100)
@@ -87,7 +106,7 @@
     //        h1.style.position = "static";
     //    }
     //    //console.log(document.body.scrollTop);
+
     //};
     //setInterval(myFraneticFunction, 10);
-})(window, document.documentElement);
-//# sourceMappingURL=app.js.map
+})(window, document.documentElement)
